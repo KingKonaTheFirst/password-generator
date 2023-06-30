562cb83dad7a4611170ef6fb5931e651304e759c
Kona <landon.deroin@gmail.com>
2023-06-30 19:05:18 -0400

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,25 @@
 var generateBtn = document.querySelector("#generate");
 
 // pw generation indexs
-var number = ['0','1','2','3','4','5','6','7','8','9'];
-var specialChar = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','}','[',']'];
-var alphabetLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var alphabetUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let number = ['0','1','2','3','4','5','6','7','8','9'];
+let specialChar = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','}','[',']'];
+let alphabetLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let alphabetUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 // Write password to the #password input
-function writePassword() {
+  function writePassword() {
+    var pwLength = prompt("How long? Choose between 8 and 128")
+      if (pwLength >=8 && pwLength <=128){
+        //pw specs
+        numbs = confirm("Would you like numbers?");
+        lower = confirm("Do you want lower case letters?");
+        upper = confirm("How about upper case letters?");
+        signs = confirm("want those weird symbols");
+
+    }
+  
+  
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
